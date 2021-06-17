47b9852e29b94ce3e6dcb0bcc4d214cb51074354
yasu_salmon <nekononikukyu0424@gmail.com>
2021-06-17 22:44:40 +0900

    ボタンにアクションを追加してみた

--- tdfm.java
+++ tdfm.java
@@ -1,20 +1,16 @@
 import java.util.*;
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
 
-public class tdfm extends JFrame{
+public class tdfm extends JFrame implements ActionListener{
 
   public tdfm(){
     this.setTitle("TodoForMe");
     this.setSize(800,600);
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     this.setLocationRelativeTo(null);
-  }
-
-  public static void main(String[] args){
-    System.out.println(" Let's start TdFM!! ");
-    tdfm frame = new tdfm();
-    Container cont = frame.getContentPane();
+    Container cont = this.getContentPane();
     JPanel panel = new JPanel();
     panel.setLayout(null);
     JLabel label = new JLabel("TodoForMe!!");
@@ -27,12 +23,21 @@ public class tdfm extends JFrame{
     button.setBounds(250,300,150,32);
     JButton button2 = new JButton("yeah2!!");
     button2.setBounds(450,300,150,64);
+    button.addActionListener(this);
     panel.add(button);
     panel.add(button2);
 
     cont.add(panel,BorderLayout.CENTER);
+  }
+
+  public static void main(String[] args){
+    System.out.println(" Let's start TdFM!! ");
+    tdfm frame = new tdfm();
+
     frame.setVisible(true);
   }
 
-
+  public void actionPerformed(ActionEvent e){
+    System.out.println("aaa");
+  }
 }
